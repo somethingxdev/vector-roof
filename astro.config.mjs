286972df23d2c343fa6a2108cf1d53279be3a04f
somethingxdev <hevnxv19@gmail.com>
2024-01-23 16:25:51 +0300

    change to SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,17 @@
-import react from '@astrojs/react';
-import sitemap from '@astrojs/sitemap';
-import tailwind from '@astrojs/tailwind';
-import icon from 'astro-icon';
-import { defineConfig } from 'astro/config';
+import react from '@astrojs/react'
+import sitemap from '@astrojs/sitemap'
+import tailwind from '@astrojs/tailwind'
+import vercel from '@astrojs/vercel/serverless'
+import icon from 'astro-icon'
+import { defineConfig } from 'astro/config'
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     tailwind({
       applyBaseStyles: false,
     }),
+    output: 'server',
+    adapter: vercel(),
     react(),
     sitemap(),
     icon(),
