209e54e6e9ed30debdb721abd6f9b55d014625dd
somethingxdev <hevnxv19@gmail.com>
2024-02-05 19:56:46 +0200

    company page

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,7 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   output: 'server',
   prefetch: {
+    prefetchAll: true,
     defaultStrategy: 'viewport',
   },
   experimental: {
